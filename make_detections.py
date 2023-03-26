2aa52f780568599ef9c96d8f4b368117eab2b298
Tomás Golvano <tgolvano@gmail.com>
2023-03-27 00:30:22 +0200

    Several joint angles calculated. MP4 video input allowed.

--- make_detections.py
+++ make_detections.py
@@ -1,9 +1,16 @@
-import torch
+# Based on the work by
+# https://github.com/nicknochnack/MediaPipePoseEstimation
+# Learning purposes
 
+
+
+import torch
+import json
 import cv2
 import mediapipe as mp
 import numpy as np
 from numpy import linalg as LA
+import os
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -26,13 +33,43 @@ def calculate_angle(a, b, c):
 
     return angle
 
+options = ['WebCam', 'mp4 video']
+
+user_input = ''
+input_message = "Pick an option:\n"
+
+for index, item in enumerate(options):
+    input_message += f'{index+1}) {item}\n'
+
+while user_input not in map(str, range(1, len(options) + 1)):
+    user_input = input(input_message)
+
+print('You picked: ' + options[int(user_input) - 1])
+
+# CAM
+if int(user_input) == 1:
+    cap = cv2.VideoCapture(0)
+
+# VIDEO MP4 format
+elif int(user_input) == 2:
+    # Set the path to your mp4 video
+    with open('config.json') as f:
+        config = json.load(f)
+    video_path = os.path.join(config['data_path'], "001.mp4")
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    # Check if the video was opened successfully
+    if not cap.isOpened():
+        print("Error opening video file")
+else:
+    print("How did you even got here?\n")
 
 
 
-cap = cv2.VideoCapture(0)
+#
 
 # Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7) as pose:
+with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7, smooth_landmarks=True) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
 
@@ -55,23 +92,46 @@ with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7) as 
             l_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
             l_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
             l_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+            l_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            l_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            l_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+            l_foot = [landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y]
 
             r_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
             r_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
             r_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            r_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            r_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            r_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+            r_foot = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y]
 
+
+# HIP
             l_hip_angle = calculate_angle(l_shoulder, l_hip, l_knee)
             r_hip_angle = calculate_angle(r_shoulder, r_hip, r_knee)
+# KNEE
+            l_knee_angle = calculate_angle(l_ankle, l_knee, l_hip)
+            r_knee_angle = calculate_angle(r_ankle, r_knee, r_hip)
+# ELBOW
+            l_elbow_angle = calculate_angle(l_shoulder, l_elbow, l_wrist)
+            r_elbow_angle = calculate_angle(r_shoulder, r_elbow, r_wrist)
+# SHOULDER
+            l_shoulder_angle = calculate_angle(l_hip, l_shoulder, l_elbow)
+            r_shoulder_angle = calculate_angle(r_hip, r_shoulder, r_elbow)
+# ANKLE DORSIFLEXION
+            l_ankle = calculate_angle(l_knee, l_ankle, l_foot)
+            r_ankle = calculate_angle(r_knee, r_ankle, r_foot)
+
 
 
             # Visualize angle
             cv2.putText(image, str(l_hip_angle), 
                            tuple(np.multiply(l_hip, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
             cv2.putText(image, str(r_hip_angle), 
                            tuple(np.multiply(r_hip, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
         except:
             pass
 
@@ -86,7 +146,7 @@ with mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7) as 
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
             
-    cap.Release()
+    cap.release()
     cv2.destroyAllWindows()
     
 
